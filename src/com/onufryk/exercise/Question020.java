e25b61d5eed28c32fa822020c8d447364ad99f4c
Roman Onufryk <roman.onufryk@gmail.com>
2016-07-24 20:35:38 -0700

    Not completed Question 020

--- src/com/onufryk/exercise/Question020.java
+++ src/com/onufryk/exercise/Question020.java
@@ -3,7 +3,64 @@ package com.onufryk.exercise;
 import com.onufryk.exercise.struct.BinaryTree;
 import com.onufryk.exercise.struct.TreeNode;
 
+/**
+ * @deprecated
+ */
 public class Question020 {
+	
+	public static class CustomBinaryTree<Item extends Comparable<Item>> extends BinaryTree<Item> {
+
+		public CustomBinaryTree() {
+			super();
+		}
+
+		public CustomBinaryTree(TreeNode<Item> root) {
+			super(root);
+		}
+		
+		public int sizeOfLargestTree() {
+			return this.sizeOfLargestTree(this.root);
+		}
+		
+		private Integer sizeOfLargestTree(TreeNode<Item> node) {
+			if (node == null) {
+				return 0;
+			}
+			
+			Integer leftSubtreeSize = sizeOfLargestTree(node.left);
+			
+			Integer rightSubtreeSize = sizeOfLargestTree(node.right);
+			
+			return leftSubtreeSize + rightSubtreeSize + 1;
+			/*
+			if (node == null) {
+				largestSize = 0;
+				min = Integer.MIN_VALUE;
+				max = Integer.MAX_VALUE;
+				return true;
+			}
+			int minLeft = Integer.MIN_VALUE, maxLeft = Integer.MAX_VALUE, leftSize = 0;
+			boolean left = this.sizeOfLargestTree(node.left, minLeft, maxLeft, leftSize);
+			
+			int minRight = Integer.MIN_VALUE, maxRight = Integer.MAX_VALUE, rightSize = 0;
+			boolean right = this.sizeOfLargestTree(node.right, minRight, maxRight, rightSize);
+			
+			boolean overall = false;
+			if (left && right && node.value.compareTo(maxLeft) == -1 && node.value.compareTo(minRight) == 1) {
+				largestSize = leftSize + rightSize + 1;
+				overall = true;
+				
+				min = (node.value < minLeft ) ? node.value : minLeft;
+				max = (node.value > maxRight) ? node.value : maxRight;
+			} else {
+				largestSize = (leftSize > rightSize) ? leftSize : rightSize;
+			}
+			
+			return overall;
+			*/
+		}
+	}
+	
 	
 	public static void main(String[] args) {
 		TreeNode<Integer> nodeA = new TreeNode<Integer>(12);
@@ -19,7 +76,7 @@ public class Question020 {
 		nodeA.left = nodeB;
 		nodeA.right = nodeC;
 
-		BinaryTree<Integer> tree = new BinaryTree<Integer>(nodeA);
+		CustomBinaryTree<Integer> tree = new CustomBinaryTree<Integer>(nodeA);
 		
 		tree.traverseInOrder();
 		
--- src/com/onufryk/exercise/struct/BinaryTree.java
+++ src/com/onufryk/exercise/struct/BinaryTree.java
@@ -89,45 +89,4 @@ public class BinaryTree<Item extends Comparable<Item>> {
 		return null;
 	}
 	
-	public int sizeOfLargestTree() {
-		return this.sizeOfLargestTree(this.root);
-	}
-	
-	private Integer sizeOfLargestTree(TreeNode<Item> node) {
-		if (node == null) {
-			return 0;
-		}
-		
-		Integer leftSubtreeSize = sizeOfLargestTree(node.left);
-		
-		Integer rightSubtreeSize = sizeOfLargestTree(node.right);
-		
-		return leftSubtreeSize + rightSubtreeSize + 1;
-		/*
-		if (node == null) {
-			largestSize = 0;
-			min = Integer.MIN_VALUE;
-			max = Integer.MAX_VALUE;
-			return true;
-		}
-		int minLeft = Integer.MIN_VALUE, maxLeft = Integer.MAX_VALUE, leftSize = 0;
-		boolean left = this.sizeOfLargestTree(node.left, minLeft, maxLeft, leftSize);
-		
-		int minRight = Integer.MIN_VALUE, maxRight = Integer.MAX_VALUE, rightSize = 0;
-		boolean right = this.sizeOfLargestTree(node.right, minRight, maxRight, rightSize);
-		
-		boolean overall = false;
-		if (left && right && node.value.compareTo(maxLeft) == -1 && node.value.compareTo(minRight) == 1) {
-			largestSize = leftSize + rightSize + 1;
-			overall = true;
-			
-			min = (node.value < minLeft ) ? node.value : minLeft;
-			max = (node.value > maxRight) ? node.value : maxRight;
-		} else {
-			largestSize = (leftSize > rightSize) ? leftSize : rightSize;
-		}
-		
-		return overall;
-		*/
-	}
 }
