5bc70b33b3d58d34cf6b92bbe4a538e86d742177
Roman Onufryk <roman.onufryk@gmail.com>
2016-08-19 22:38:32 -0700

    Question 63

--- src/com/onufryk/exercise/Question063.java
+++ src/com/onufryk/exercise/Question063.java
@@ -1,37 +1,60 @@
 package com.onufryk.exercise;
 
 public class Question063 {
-
-	public static boolean isBST(int[] postOrder) {
-		return isBST(postOrder, 0, postOrder.length - 1);
-
+	private int[] sequence = {5, 7, 6, 9, 11, 10, 8};
+	
+	public void run() {
+		if (isBST()) { 
+			System.out.println("BINARY SEARCH TREE");
+		} else {
+			System.out.println("NOT A BINARY SEARCH TREE");
+		}
 	}
 
-	private static boolean isBST(int[] sequence, int start, int end) {
-		int rootValue = sequence[end];
-		System.out.println("Bounds:" + start + ", " + end);
+	public boolean isBST() {
+		return isBST(0, this.sequence.length - 1);
+	}
 
-		int ii = start;
-		while (ii < end && sequence[ii] < rootValue) {
-			ii++;
+	private boolean isBST(int startIndex, int endIndex) {
+		System.out.println("---------------------------------------------------------------------------------");
+		System.out.printf("Function called with bounds %d - %d\n", startIndex, endIndex);
+
+		int rootValue = sequence[endIndex];
+		System.out.printf("Subtree root value is %d\n", rootValue);
+		System.out.printf("Values are %d, %d\n", this.sequence[startIndex], this.sequence[endIndex - 1]);
+
+
+		int ii = 0;
+		for (ii = startIndex; ii < endIndex - 1; ++ii) {
+			if (this.sequence[ii] > rootValue) {
+				break;
+			}
 		}
-
-		int jj = end - 1;
-		while (jj > start && sequence[ii] > rootValue) {
-			jj--;
+		
+		int jj = 0;
+		for (jj = ii; jj < endIndex - 1; jj++) {
+			if (this.sequence[jj] < rootValue) {
+				return false;
+			}
 		}
-
-		System.out.println("Indexes:" + ii + ", " + jj);
-
-		if (jj - ii > 1) {
-			return false;
+		
+		boolean left = true;
+		if (ii > 0) {
+			left = isBST(0, ii);
 		}
-		return isBST(sequence, start, ii) && isBST(sequence, jj, end - 1);
+		
+		boolean right = true;
+		
+		if (ii < endIndex - 1) {
+			right = isBST(ii, endIndex - ii - 1);
+		}
+		
+		return left && right;
 	}
 
 	public static void main(String[] args) {
-		int[] postOrder = { 5, 7, 6, 9, 11, 10, 8 };
-		System.out.println(isBST(postOrder));
+		Question063 app = new Question063();
+		app.run();
 	}
 
 }
